51ef7554b9655078cf4224c8cfcc114613cb5928
Nishan Niraula <nrlnishan@gmail.com>
2025-01-03 11:19:43 +0700

    Mock sdk with stub target

--- Package.swift
+++ Package.swift
@@ -4,25 +4,33 @@
 import PackageDescription
 
 let package = Package(
-    name: "DepTest",
+    name: "AmitySDK",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "DepTest",
-            targets: ["DepTest"]),
+            name: "AmitySDK",
+            targets: ["DepTest"]), // Dummy Targets
     ],
     dependencies: [
         .package(url: "https://github.com/realm/realm-swift.git", exact: "10.49.3")
         
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "AmitySDK",
+            url: "https://sdk.amity.co/sdk-release/ios-frameworks/6.47.1/AmitySDK.xcframework.zip",
+            checksum: "6ffd3297479071174b3a4904cd77435b811f1b4d108b7e2066b404bb0463f76b"
+        ),
         .target(
             name: "DepTest",
             dependencies: [
+                .target(name: "AmitySDK"),
                 .product(name: "RealmSwift", package: "realm-swift")
-            ]
+            ],
+            path: "Sources"
         ),
     ]
 )
